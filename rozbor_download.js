4f94dfd81bdf3a083af9d6111863561ed604f055
TedRomato <59472129+TedRomato@users.noreply.github.com>
2023-01-28 19:28:41 +0100

    Update rozbor_download.js

--- rozbor_download.js
+++ rozbor_download.js
@@ -1,6 +1,6 @@
 
 function formatContent(htmlBody) {
-	let contentRegex = /(?=Nahlásit chybu\/nízkou kvalitu<\/strong><\/a>[\r\n \t]*<\/th>[\r\n \t]*<\/tr>[\r\n \t]*<\/thead>[\r\n \t]*<\/table>[\r\n \t]*<\/div>[\r\n \t]*([\s\S]*))/gm;
+	let contentRegex =/alt="rozbor-d[\S]la"><\/p>((?:.|\n)*)<!-- AI CONTENT END 1 -->/gm
 	let content = contentRegex.exec(htmlBody)[1];
 	content = content.replace(/<!-- AI CONTENT END 1 -->[\s\S]*/gm, ''); 
 	content = content.replace(/<ins[^>]+?>/gm, '');
